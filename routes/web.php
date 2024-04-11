86d0ffdb74804d51391cba54d601524c4d3b851b
Agathapolati <agathapolati1@gmail.com>
2024-04-11 15:03:05 -0300

    Feat Laravel: telas de login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,12 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::view('/', 'welcome');
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-Route::post('/send', [ContatoController::class, 'send'])->name('contato.send');
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
